ff3fec059710cffe3c0a22ab883c1106c6489b38
aedi <none>
2016-06-03 21:23:19 -0500

    Javascript: Vaciar input y select de principal.xhtml

--- WebContent/js/funciones.js
+++ WebContent/js/funciones.js
@@ -3,4 +3,11 @@ var len=selects.length;
 
 for (var i=0; i< len; i++){
 selects[i].setAttribute("class","ui-selectonemenu-trigger ui-state-default ui-corner-right");
+selects[i].value=-1;
+}
+
+var inputs=document.querySelectorAll("input[type=text]");
+len=inputs.length;
+for(var i=0; i<len; i++){
+	inputs[i].value="";
 }
